f0cc35ac6cf3ca869c8df5db1c70c9a7af1929e7
iVerkhov <vrkhv@iCloud.com>
2023-10-22 20:28:23 +0300

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Игорь Верхов on 22.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
